f238857be1579d8c4bd01da26e8bcc74dce395d6
chenc <chenc@fengyuntec.com>
2020-11-05 17:59:09 +0800

    chore: 依赖修改

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+export default {
+    input: 'components/index.ts',
+    output: {
+        file: 'lib/index.js',
+        name: 'ui',
+        format: 'umd',
+        sourcemap: true,
+    },
+    plugins: [
+    ],
+};
